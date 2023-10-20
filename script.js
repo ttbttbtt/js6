ccc88fa3bf45c4cee11702beb76f62ccf49c37b2
ttbttbtt <docent2212@mail.ru>
2023-10-20 14:21:48 +0300

    clean

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-// ввод названия фильма
-let vvod = document.querySelector('.vvod')
+
+let vvod = document.querySelector('.vvod')             // ввод названия фильма
+
+let btn = document.getElementById('btn')               // кнопка Найти
+
+let poster = document.querySelector('.poster')         // обёртка постеров
+
+let imgOld = document.getElementsByTagName('img')      // предыдущие постеры
+
+let info = document.getElementsByClassName('info')[0]  // вывод результата
 
 
-// кнопка Найти
-let btn = document.getElementById('btn')
+// очищение предыдущего
+function clean() {
+    // очищение описания
+    info.innerHTML = ''
 
-
-// вывод результата
-let info = document.getElementsByClassName('info')[0]
-
-
-// постер
-let poster = document.querySelector('.poster')
-
+    // удаление предыдущих постеров
+    for (let elem of imgOld) {
+        elem.remove();
+    }
+}
 
 // слушатель события click на кнопку Найти
 btn.addEventListener('click', function() {
@@ -21,6 +28,7 @@ btn.addEventListener('click', function() {
         seek(vvod.value)
     }
     else {
+        clean()
         console.log("НЕ Введено название фильма!")
         info.innerHTML = "НЕ Введено название фильма!"
     }
@@ -29,25 +37,20 @@ btn.addEventListener('click', function() {
 
 // показ результата на экране
 function renderInfo(data) {
-    info.innerHTML = ''
+    clean()
 
+    // вывод описания фильма
     for (let key in data) {
         info.innerHTML += `${key}: ${data[key]}<br>`
     }
-
-    let p = data["Poster"] // адрес постера
-
-    let imgOld = document.getElementsByTagName('img')
-
-    for (let elem of imgOld) {
-            elem.remove();
-    }
-
+    
+    // вывод постера
     img = document.createElement("IMG")
     // img.src = "https://m.media-amazon.com/images/M/MV5BYTU1ZTI0YjUtZGRlMS00MDU1LWFmZmItZWRiYTg5NTExMmRjXkEyXkFqcGdeQXVyMTUzMDg3MTQw._V1_SX300.jpg"
+    let p = data["Poster"] // адрес постера
     img.src = `${p}`
+    img.alt = 'Здесь должен быть постер, но его нет ('
     poster.appendChild(img)
-
 }
 
 
@@ -68,7 +71,6 @@ async function seek(vv) {
 // функция поиска на OMDB API (80437e5b)
 async function sendRequest(vv) {
     url = `https://www.omdbapi.com/?apikey=80437e5b&t=${vv}`
-
     let response = await fetch(url)
     response = await response.json()
     return response
